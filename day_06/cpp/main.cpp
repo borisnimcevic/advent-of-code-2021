ac261d051887b50b8b21a494178a9a52a5db7412
Boris Nimcevic <borisnimcevic@gmail.com>
2021-12-06 21:11:43 +0100

    part 2 day 06

--- day_06/cpp/main.cpp
+++ day_06/cpp/main.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
-#include <array>
+#include <map>
 
 uint32_t part1();
-uint32_t part2();
+uint64_t part2();
 
 const std::string inputFile = "../input/input.txt";
 /* const std::string inputFile = "../input/test.txt"; */
@@ -38,8 +38,8 @@ class Lenternfish{
 
 int main()
 {
-  std::cout << part1() <<  " <<<<< part1" << std::endl;
-  /* std::cout << part2() <<  " <<<<< part2" << std::endl; */
+  std::cout << part1() <<  " <<<<< part1 (the bad)" << std::endl;
+  std::cout << part2() <<  " <<<<< part2 (and the good)" << std::endl;
 }
 
 void printDay(const std::vector<Lenternfish> &fish, const uint32_t day)
@@ -50,6 +50,26 @@ void printDay(const std::vector<Lenternfish> &fish, const uint32_t day)
     std::cout << f.status() << ", ";
   }
   std::cout << std::endl;
+}
+
+void printFish(std::map<uint8_t, uint64_t> fish,const uint32_t day)
+{
+  std::cout << "After day " << day << "\n";
+  for(int i = 0; i < 9; ++i)
+  {
+    /* std::cout << i << ": " << fish.count(i) << std::endl; */
+    std::cout << i << ": " << fish[i] << std::endl;
+  }
+}
+
+uint64_t countFish(std::map<uint8_t, uint64_t> fish)
+{
+  uint64_t sum = 0;
+  for(int i = 0; i < 9; ++i)
+  {
+    sum+=fish[i];
+  }
+  return sum;
 }
 
 uint32_t  part1()
@@ -99,7 +119,43 @@ uint32_t  part1()
 }
 
 
-uint32_t part2()
+uint64_t part2()
 {
-  return 0;
+  // ------ read file
+  std::ifstream file(inputFile);
+
+  if(!file)
+  {
+    std::cerr<<"Cannot find the file."<< std::endl;
+    return false;
+  }
+
+  std::string temp;
+  file >> temp;
+  std::map<uint8_t, uint64_t> fish;
+
+  std::stringstream ss(temp);
+  for(int i; ss >> i;)
+  {
+    fish[i]++;
+    if(ss.peek() == ',')
+    {
+      ss.ignore();
+    }
+  }
+
+  // --- update fish count
+  const auto DAYS = 256;
+  for(int day = 1; day <= DAYS; ++day)
+  {
+    uint64_t temp = fish[0];
+    for(int i = 1; i < 9; ++i)
+    {
+      fish[i-1] = fish[i];
+    }
+    fish[6] += temp;
+    fish[8] = temp;
+  }
+
+  return countFish(fish);
 }
